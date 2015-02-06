fb02010c5a998335367a221319e3aa4c55fc492d
Gordon Carrie <dev0@trekix.net>
2015-02-06 14:29:58 -0600

    -d option now specifies fragment type, not title

--- test/pisa.exp.sh
+++ test/pisa.exp.sh
@@ -7,7 +7,7 @@ awk 'BEGIN {
     }
 }' \
 | polyline 'id="expm" stroke="black" stroke-width="0.01" fill="none"'	\
-| pisa -d "Plot exp(-x)" -l 40 -r 40 -t 40 -b 40 -w 800 -f 12		\
+| pisa -l 40 -r 40 -t 40 -b 40 -w 800 -f 12		\
     -p prefix.svg -s suffix.svg -y pisa.css -X 'x' -Y 'exp(-x)'	\
     -- 0.0 3.0 1.0 -1.0
 
